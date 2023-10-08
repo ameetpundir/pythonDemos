2940f1bb00dcb3fa546e9fdc3ce1c7211bdc9ad8
Amit <ameetpundir@gmail.com>
2023-10-08 12:27:35 +0200

    nothing much added

--- Madlibs.py
+++ Madlibs.py
@@ -1,18 +1,19 @@
 # string concatenation (aka how to put strings together)
 # suppose we want to create a string that says  "Hello you , _______ "
 
-user = "AP" # some string variable
+user = "AP"  # some string variable
 
 # a few ways to do it
 print("Hello you, "+user)
 print("Hello you, {}".format(user))
 print(f"Hello you, {user}")
-print("Hello you, %s" %(user))
+print("Hello you, %s" % (user))
 
 
 adj = input("Adjective : ")
 verb1 = input("verb :")
 madjib = f"alskjfljd {adj}! sldjlajsdf fsd\
 ldasjflj {verb1}"
-    
-print (madjib)
+
+print(madjib)
+print(madjib)
